7ecbcf6014578ae878dfe5dbd53e15518d4a7b92
ValentinaKry <98461511+ValentinaKry@users.noreply.github.com>
2022-12-07 14:37:09 +0300

    Add files via upload

---
+++ iOS App Dev Tutorials/iOS App Dev Tutorials/iOS_App_Dev_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_App_Dev_TutorialsApp.swift
+//  iOS App Dev Tutorials
+//
+//  Created by Valentina Krysak on 7.12.22.
+//
+
+import SwiftUI
+
+@main
+struct iOS_App_Dev_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
